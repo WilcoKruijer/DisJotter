d2c84a42a43575ff3fb0fa152ddeb7016619296c
skoulouzis <S.Koulouzis@uva.nl>
2020-12-22 18:19:35 +0100

    added more config examples

--- fair-cells/helper/util.py
+++ fair-cells/helper/util.py
@@ -2,5 +2,7 @@ import json
 
 
 def get_config():
-    with open('nb_helper_config.json', 'r') as cfg:
+    path = 'nb_helper_config_empty.json'
+    path = '../docker/helper_dummy/nb_helper_config_empty.json'
+    with open(path, 'r') as cfg:
         return json.load(cfg)
