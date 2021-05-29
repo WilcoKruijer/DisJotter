9832262484578c488bb747243368280dfe448fcc
skoulouzis <skoulouzis@users.noreply.github.com>
2021-05-29 11:52:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="FAIR-Cells",
-    version="1.0.16",
+    version="1.0.17",
     author="Wilco Kruijer, Zhiming Zhao",
     author_email="Z.Zhao@uva.nl",
     description=short_desc,
