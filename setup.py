fbfd807ee0181df4b49363a575792fa1c7c56d2a
skoulouzis <S.Koulouzis@uva.nl>
2021-06-03 17:47:24 +0200

    disable conda

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="FAIR-Cells",
-    version="1.0.19",
+    version="1.0.20",
     author="Wilco Kruijer, Zhiming Zhao",
     author_email="Z.Zhao@uva.nl",
     description=short_desc,
