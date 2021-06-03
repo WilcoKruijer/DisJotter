6ec5b6c94d7d3a37cc57cc390763bdf302fe36a9
skoulouzis <S.Koulouzis@uva.nl>
2021-06-03 17:47:04 +0200

    disable conda

--- fair-cells/backend/container/docker_service.py
+++ fair-cells/backend/container/docker_service.py
@@ -40,7 +40,9 @@ class DockerService:
 
                 f'COPY ./fair-cells/ /src/fair-cells/',
                 f"RUN pip install -r /src/fair-cells/helper/helper_requirements.txt",
-                f"RUN pip install -r requirements.txt",
+
+                f'COPY ./requirements.txt /src/requirements.txt',
+                f"RUN pip install -r /src/requirements.txt",
                 # f'COPY ./environment.yml /src/environment.yml',
                 # f"RUN conda env update --file environment.yml --name base",
 
